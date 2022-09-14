7cd225a64f38128120ac5da45ff9bb0a320c30ef
Appiva-Tech <brscntyz@gmail.com>
2022-09-14 20:24:30 +0300

    difficulty part

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use blockchainlib::*;
+
 fn main () {
-    println!("Hello, blockchain!");
+    let mut block = Block::new(0,0,vec![0;32],0,"Genesis Block!".to_owned(),
+    0x0000ffffffffffffffffffffffffffff);
+
+    block.hash = block.hash();
+
+    println!("{:?}",block);
+
+    block.mine();
+
+    println!("{:?}",&block);
+
+
+
 }
+
