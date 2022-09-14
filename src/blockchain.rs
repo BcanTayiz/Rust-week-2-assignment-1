7a84d48f46d533c8b9ae3cd0ddc303e352b4fa71
Appiva-Tech <brscntyz@gmail.com>
2022-09-14 21:28:47 +0300

    blockchain creation

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -3,5 +3,28 @@ use super::*;
 
 pub struct Blockchain{
     pub blocks: Vec<Block>,
+}
 
+impl Blockchain{
+    pub fn verify(&self) -> bool {
+        for(i,block) in self.blocks.iter().enumerate(){
+            if block.index != i as u32 {
+                print!("Index missmatch! {} != {}",&block.index,&i);
+                return false;
+            } else if !block::check_difficulty(&block.hash(),block.difficulty){
+                println!("Difficulty fail");
+                return false;
+            } else if i != 0{
+                // Not genesis block
+            } else {
+                //Genesis block
+                if block.prev_block_hash != vec![0;32]{
+                    print!("Genesis block prev_block_hash invalid")
+                    return false
+                }
+            }
+        };
+
+        return false;
+    }
 }
