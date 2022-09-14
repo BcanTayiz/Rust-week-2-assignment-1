d6f272260ad2169e6b16650f4c415ca0a9ebfc3f
Appiva-Tech <brscntyz@gmail.com>
2022-09-14 23:18:46 +0300

    transaction enum error types are set

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,38 +1,44 @@
 
 use super::*;
 
+pub enum BlockValidationErr {
+    MismatchedIndex,
+    InvalidHash,
+    AchronologicalTimestamp,
+    MismatchedPreviousHash,
+    InvalidGenesisBlockFormat,
+    InvalidInput,
+    InsufficientInputValue,
+    InvalidCoinbaseTransaction,
+}
+
 pub struct Blockchain{
     pub blocks: Vec<Block>,
 }
 
 impl Blockchain{
-    pub fn verify(&self) -> bool {
-        for(i,block) in self.blocks.iter().enumerate(){
+    pub fn update_with_block(&mut self, block:Block) -> Result<(),BlockValidationErr> {
+        let i = self.blocks.len();
             if block.index != i as u32 {
-                print!("Index missmatch! {} != {}",&block.index,&i);
-                return false;
+                return Err(BlockValidationErr::MismatchedIndex);
             } else if !block::check_difficulty(&block.hash(),block.difficulty){
-                println!("Difficulty fail");
-                return false;
+                return Err(BlockValidationErr::InvalidHash);
             } else if i != 0{
                 // Not genesis block
                 let prev_block = &self.blocks[i-1];
                 if block.timestamp <= prev_block.timestamp{
-                    println!("Time did not increase");
-                    return false;
+                    return Err(BlockValidationErr::AchronologicalTimestamp)
                 } else if block.prev_block_hash != prev_block.hash{
-                    println!("Hash mismatch!");
-                    return false;
+                    return Err(BlockValidationErr::MismatchedPreviousHash)
                 }
             } else {
                 //Genesis block
                 if block.prev_block_hash != vec![0;32]{
-                    print!("Genesis block prev_block_hash invalid");
-                    return false;
+                    return Err(BlockValidationErr::InvalidGenesisBlockFormat)
                 }
             }
-        };
 
-        true
-    }
+            Ok(())
+        }
+
 }
